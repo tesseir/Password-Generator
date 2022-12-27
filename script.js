ddd23e762409c771cea8fe860f64541d3f75feb0
tesseir <tesseir.edu@gmail.com>
2022-12-27 01:55:34 -0600

    JS initial Commit

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
-// ! http://www.net-comber.com/charset.html
+//! https://www.w3schools.com/charsets/ref_html_ascii.asp
 
-function randomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
+//? template for the range, lines 19-30 will populate this.
+const setRange = (start, end) => {
+  let result = [];
+  for (let i = start; i <= end; i = i++) { result.push(i); }
+  return result;
+};
 
-console.log(randomUpper());
+//? ... it a microwave... ITS A RANDOMIZER WHAT DID YOU THINK IT WAS?
+const randomizer = (range) => {
+  return range[Math.floor(Math.random() * range.length)];
+};
 
-function randomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
+//? array of password parameters
+const passwdParam = (passLgth, useUp, useLow, useNum, useSpec) => {
 
-console.log(randomLower());
+  //? populates the range template and uses the ranzomizer to pick a number. 
+  const genRandomizer = {
+    upperCase: () => randomizer(setRange(65, 90)),
+    lowerCase: () => randomizer(setRange(97, 122)),
+    num: () => randomizer(setRange(48, 57)),
+    specChar: () => randomizer([
+      ...setRange(33, 47),
+      ...setRange(58, 64),
+      ...setRange(91, 96),
+      ...setRange(123, 126)
+    ])
+    //! "..." allows you to put multiple ranges together into one big range. 
+  };
 
-function randomNum() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+  let charArray = [];
+  let usedGen = [];
 
-console.log(randomNum());
+  //? password parameters true or false
+  if (useUp) {
+    usedGen.push(genRandomizer.upperCase);
+  } if (useLow) {
+    usedGen.push(genRandomizer.lowerCase);
+  } if (useNum) {
+    usedGen.push(genRandomizer.num);
+  } if (useSpec) {
+    usedGen.push(genRandomizer.specChar);
+  }
 
-function randomSpecChar() {
-  const specChar = "!@#$%^&*(){}[]_-+=/,.<>?";
-  return specChar[Math.floor(Math.random() * specChar.length)];
-}
+  //? sets password length, and randomizes given integers, while converting to ascii characters.
+  for (let i = 0; i < passLgth; i++) {
+    const selectedGenerator = randomizer(usedGen);
 
-console.log(randomSpecChar());
+    charArray.push(String.fromCharCode(selectedGenerator()));
+  }
+
+  //? joins together randomly generated characters
+  console.log(charArray);
+  return charArray.join("");
+};
+
+const pass = passwdParam(25, true, true, true, true);

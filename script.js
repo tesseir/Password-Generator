ce36d488b1bf6e93f688063728438e328d92328d
tesseir <tesseir.edu@gmail.com>
2022-12-26 19:29:24 -0600

    uhhh

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-// ! Generator Functions - http://ww.net-comber.com/charset.html
+// ! http://www.net-comber.com/charset.html
 
+function randomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+console.log(randomUpper());
+
+function randomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(randomLower());
+
+function randomNum() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(randomNum());
+
+function randomSpecChar() {
+  const specChar = "!@#$%^&*(){}[]_-+=/,.<>?";
+  return specChar[Math.floor(Math.random() * specChar.length)];
+}
+
+console.log(randomSpecChar());
